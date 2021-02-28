ed3a9f37d1d9a82cea362ef699eb3ad3abeabc21
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2021-02-28 23:23:48 +0200

    Set the State transfer path App ->BusinessList -> Business

--- src/Components/BusinessList/BusinessList.jsx
+++ src/Components/BusinessList/BusinessList.jsx
@@ -3,17 +3,15 @@ import './BusinessList.css';
 import Business from '../Bussiness/Bussiness';
 
 export default class BusinessList extends React.Component {
-
-    render(){
-        return (
-            <div className="BusinessList">
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-          </div> 
-        );
+    render() {
+      return (
+        <div className="BusinessList">
+          {
+            this.props.businesses.map(business => {
+              return <Business business={business} />
+            })
+          }
+        </div>
+      );
     }
-}
+  }
