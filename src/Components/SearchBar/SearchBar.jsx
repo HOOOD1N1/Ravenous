df680b825d8ffd61b6d55d6d342dbdcfe3d35bca
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2021-02-28 23:43:24 +0200

    SearchBar: handleSortByChange + getSortByClass methods

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import './SearchBar.css';
-const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
-};
+
 export default class SearchBar extends React.Component {
     constructor(props){
         super(props);
         this.renderSortByOptions = this.renderSortByOptions.bind(this);
+        this.getSortByClass = this.getSortByClass.bind(this);
+        this.state = {
+            'term': '',
+            'location': '',
+            'sortBy': 'best_match'
+        };
+    
+        this.sortByOptions = {
+            'Best Match': 'best_match',
+            'Highest Rated': 'rating',
+            'Most Reviewed': 'review_count'
+        };
+
+    }
+    getSortByClass(sortByOption){
+        if(this.state.sortBy === sortByOption) {
+            return 'active';
+        }
+        return '';
     }
     renderSortByOptions(){
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
+            return <li className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption}</li>
         });
+    }
+    handleSortByChange(sortByOption){
+        this.setState({sortBy: sortByOption});
+
     }
     
 
